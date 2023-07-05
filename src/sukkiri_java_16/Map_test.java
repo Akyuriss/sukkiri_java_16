f784dc3b919fbf85de13f014959c35b020bc118b
Akyuriss <135511100+Akyuriss@users.noreply.github.com>
2023-07-05 23:08:43 +0900

    16章完了

--- src/sukkiri_java_16/Map_test.java
+++ src/sukkiri_java_16/Map_test.java
@@ -1,12 +1,13 @@
 package sukkiri_java_16;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Map_test {
 	public static void main(String[] args) {
 		Map<String, Integer> prefs = new HashMap<String, Integer>();
-		prefs.put("京都府",255); //$NON-NLS-1$
-		prefs.put("東京都",300); //$NON-NLS-1$
-		prefs.put("熊本", 400); //$NON-NLS-1$
+		prefs.put("京都府",255); 
+		prefs.put("東京都",300); 
+		prefs.put("熊本", 400); 
 		
 		int Tokyo = prefs.get("東京都"); //$NON-NLS-1$
 		System.out.println(Tokyo);
